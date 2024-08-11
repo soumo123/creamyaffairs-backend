6abf67748ab17a9bd844c7af6a290500078428e0
Soummya Biswas <soummyabiswas999@gmail.com>
2024-08-11 20:35:37 +0530

    env change

--- server/api/middleware/aws.config.js
+++ server/api/middleware/aws.config.js
@@ -4,8 +4,8 @@ dotenv.config();
 
 
 AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_KEY,
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_KEY,
     region: process.env.region,
 });
 
